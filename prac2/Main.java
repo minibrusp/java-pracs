3722e4a7ea087328f0f7dffe0baae2da9b6e60d4
XIII <minibrusp@gmail.com>
2024-02-29 03:22:56 +0800

    prac2

---
+++ prac2/Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Main {
+  public static void main(String arg[]) {
+    System.out.println("Hello");
+
+    // Collection nums = new Collection();
+
+    // Collection<Integer> nums = new ArrayList<Integer>();
+    List<Integer> nums = new ArrayList<Integer>();
+    nums.add(6);
+    nums.add(5);
+    nums.add(8);
+    nums.add(2);
+
+
+    // // Collection Interface
+    // System.out.println(nums);
+
+    // // Collection I
+    // for (int n : nums) {
+    //   System.out.println(n);
+    // }
+
+    // List
+    System.out.println(nums.get(2));
+    System.out.println(nums.indexOf(8));
+    System.out.println(nums.contains(5));
+
+    
+
+
+  }
+}
