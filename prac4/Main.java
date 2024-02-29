23c3fcdd2889e09cc32a2ae028d4002aad295ece
XIII <minibrusp@gmail.com>
2024-02-29 11:01:18 +0800

    prac4

--- prac4/Main.java
+++ prac4/Main.java
@@ -37,7 +37,7 @@ public class Main {
 
     // mathSubject.displayStudents();
     
-    // scienceSubject.displayStudents();
+    scienceSubject.displayStudents();
 
     mathSubject.displayGrades();
 
